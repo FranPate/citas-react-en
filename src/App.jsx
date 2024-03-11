f7c3805d1a3af7206babd702ce6b615555e6ff32
Francisco Patejim <jimpate0@gmail.com>
2024-03-11 17:14:54 -0300

    Solving error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Form from './components/Form'
 import PatientList from './components/PatientList'
@@ -6,6 +6,19 @@ import PatientList from './components/PatientList'
 function App() {
   const [patients, setPatients] = useState([])
   const [patient, setPatient] = useState({})
+
+  useEffect(() => {
+    const obtainLS = () => {
+      const patientsLS = JSON.parse(localStorage.getItem('patients')) ?? [];
+
+      setPatients(patientsLS)
+    }
+    obtainLS()
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('patients', JSON.stringify( patients ))
+  }, [patients])
 
   const deletePatient = id => {
     const updatedPatients = patients.filter( patient => patient.id !== id );
